d25e2312d6f6c37801e81c644d364c6c390d722c
Jackob2004 <zjakub2004@gmail.com>
2026-07-29 11:10:28 +0200

    feat(web): add config, error handling and dependency organization
    Add config value via command line flag.
    Add custom logger.
    Make dependencies available through application struct.
    Centralize error handling.

---
+++ cmd/web/helpers.go
@@ -0,0 +1,17 @@
+package main
+
+import "net/http"
+
+func (a *application) serverError(w http.ResponseWriter, r *http.Request, err error) {
+	var (
+		method = r.Method
+		uri    = r.RequestURI
+	)
+
+	a.logger.Error(err.Error(), "method", method, "uri", uri)
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+}
+
+func (a *application) clientError(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
+}
