24be35f84171d047d6f3521d65b758a643100c35
Jackob2004 <zjakub2004@gmail.com>
2026-08-08 15:07:29 +0200

    feat(ui): embed static and dynamic files

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,14 +3,14 @@ package main
 import (
 	"net/http"
 
+	"github.com/Jackob2004/snippetbox/ui"
 	"github.com/justinas/alice"
 )
 
 func (a *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+	mux.Handle("GET /static/", http.FileServerFS(ui.Files))
 
 	dynamic := alice.New(a.sessionManager.LoadAndSave, preventCSRF, a.authenticate)
 
--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"html/template"
+	"io/fs"
 	"net/http"
 	"path/filepath"
 	"time"
 
 	"github.com/Jackob2004/snippetbox/internal/models"
+	"github.com/Jackob2004/snippetbox/ui"
 	"github.com/justinas/nosurf"
 )
 
@@ -40,7 +42,7 @@ var functions = template.FuncMap{
 func newTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
 
-	pages, err := filepath.Glob("./ui/html/pages/*.gohtml")
+	pages, err := fs.Glob(ui.Files, "html/pages/*.gohtml")
 	if err != nil {
 		return nil, err
 	}
@@ -48,17 +50,13 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.gohtml")
-		if err != nil {
-			return nil, err
+		patterns := []string{
+			"html/base.gohtml",
+			"html/partials/*.gohtml",
+			page,
 		}
 
-		ts, err = ts.ParseGlob("./ui/html/partials/*.gohtml")
-		if err != nil {
-			return nil, err
-		}
-
-		ts, err = ts.ParseFiles(page)
+		ts, err := template.New(name).Funcs(functions).ParseFS(ui.Files, patterns...)
 		if err != nil {
 			return nil, err
 		}
